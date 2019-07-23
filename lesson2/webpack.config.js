260a83b992892728962d47df1f84d1830e6abb2e
hydra13 <rob.sabirov@yandex.ru>
2019-07-23 12:02:36 +0300

    Lesson 2

---
+++ lesson2/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+module.exports = {
+    entry: './src/App.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    }
+}
