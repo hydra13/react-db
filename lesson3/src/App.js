725659a48ec62ef0d65a81f4f690452b2e65e3dd
hydra13 <rob.sabirov@yandex.ru>
2019-07-30 14:41:57 +0300

    Menu was completed (+ used Bulma)

--- lesson3/src/App.js
+++ lesson3/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Login from './Login'
+import './App.scss';
+import Login from './Login';
+import Menu from './Menu';
 
 class App extends React.Component {
     render() {
+        const leftItems = [{ title: 'Home', href: '/' }, { title: 'Documentation', href: '/docs' }, { title: 'About', href: '/about' }];
+        const rightItems = [{ title: 'Log in', href: '/login' }];
         return (
             <div>
+                <Menu leftItems={leftItems} rightItems={rightItems} />
                 <Login />
             </div>
         )
